b5640fa3c9c2d1212497df91e6bc83d58d26a908
khwh111 <kelvinhung.register@gmail.com>
2014-04-22 06:24:45 +0000

    i

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -39,11 +39,6 @@
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
   <head>
     <meta charset="utf-8">
-    <!--
-
-    by Kelvin
-
-    -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
